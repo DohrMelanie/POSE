939b6dcb6bc3bfc975cf626a03c83a8b110fdc2f
Melanie Dohr <m.dohr@students.htl-leonding.ac.at>
2025-09-12 14:19:07 +0200

    added template

---
+++ homework/01-calculator/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace _01_calculator.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ homework/01-calculator/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace _01_calculator.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
