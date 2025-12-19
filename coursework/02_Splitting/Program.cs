6ba90b89a501d6fece8db13c93fdb8beda8788f1
Melanie Dohr <m.dohr@students.htl-leonding.ac.at>
2025-12-19 11:33:26 +0100

    added starter for wishlist

---
+++ coursework/02_Splitting/Program.cs
@@ -0,0 +1,15 @@
+﻿var text = """
+    key1=value1
+    key2=value2
+    ---
+    header1,header2,header3
+    data1,data2,data3
+    data4,data5,data6
+""";
+
+var topLevelParts = text.Split("---\n");
+if (topLevelParts.Length != 2)
+{
+    throw new Exception("Invalid input");
+}
+
